d86b4af4ea7a2d40530a07d86ce1b20fa8344da9
sehumah <sehumah@protonmail.com>
2022-03-23 19:13:35 -0500

    add activity description

--- app/src/main/java/com/assignment5/instagram/MainActivity.kt
+++ app/src/main/java/com/assignment5/instagram/MainActivity.kt
@@ -3,6 +3,11 @@ package com.assignment5.instagram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+/**
+ * Let a user create a post by taking a photo with their camera
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
