615552c126cb5d655179973c797f4e9bdccdd55b
sehumah <sehumah@protonmail.com>
2022-03-24 00:37:16 -0500

    inflate the app bar with the menu item and handle clicks on the logout menu item

--- app/src/main/java/com/assignment5/instagram/MainActivity.kt
+++ app/src/main/java/com/assignment5/instagram/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.assignment5.instagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 
 /**
@@ -12,5 +16,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    // inflate the resource file to be used and return true to show the menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout) {
+            // navigate to login/signup screen
+
+            // todo: navigating to login activity doesn't seem to be working, figure it out later
+            // val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            // startActivity(intent)
+
+            // just implement a toast for now
+            Toast.makeText(this, "Logout button clicked!", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
