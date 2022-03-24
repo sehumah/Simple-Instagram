dfe686678b5b54611c9ac6cec5768211ad3d17fd
sehumah <sehumah@protonmail.com>
2022-03-23 20:17:59 -0500

    initialize Parse for the Android app can start talking to the Parse server

--- app/src/main/java/com/assignment5/instagram/Application.kt
+++ app/src/main/java/com/assignment5/instagram/Application.kt
@@ -1,5 +1,25 @@
 package com.assignment5.instagram
 
-class Application {
+import android.app.Application
+import com.parse.Parse
 
+
+/**
+ * This class gets called whenever the application is launched for the first time
+ *
+ */
+
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // initialize Parse for the Android app can start talking to the Parse server
+        Parse.initialize(
+            Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_application_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        )
+    }
 }
