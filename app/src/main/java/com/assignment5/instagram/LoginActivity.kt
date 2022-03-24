1d6f9a8c8bce6e69964a9d86b4437a1a73ab9c5b
sehumah <sehumah@protonmail.com>
2022-03-23 19:17:02 -0500

    create new login activity

---
+++ app/src/main/java/com/assignment5/instagram/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.assignment5.instagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+/**
+ * Contains the UI and logic to let the user login to the Parse server
+ */
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
