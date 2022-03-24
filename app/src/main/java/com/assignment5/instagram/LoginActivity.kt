1fd7c8b593eca1ca6fd1010d928bda6423ccfebc
sehumah <sehumah@protonmail.com>
2022-03-23 19:38:56 -0500

    set click listener to login button and grab username and password for later logging in the user

--- app/src/main/java/com/assignment5/instagram/LoginActivity.kt
+++ app/src/main/java/com/assignment5/instagram/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.assignment5.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 
 /**
@@ -12,5 +14,17 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // set click listener to login button & grab the username and password
+        findViewById<Button>(R.id.btn_login_button).setOnClickListener {
+            val etUsername = findViewById<EditText>(R.id.et_username).text.toString()
+            val etPassword = findViewById<EditText>(R.id.et_password).text.toString()
+            loginUser(etUsername, etPassword)
+        }
+    }
+
+    // make network call to log in the user if they have the right info and already have an account
+    private fun loginUser(etUsername: String, etPassword: String) {
+        // TODO("implement functionality to log the user in to the app")
     }
 }
