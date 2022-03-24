4781d53927ae9d7f9c764e4037cf3ccaccf8e5ba
sehumah <sehumah@protonmail.com>
2022-03-24 01:25:43 -0500

    implement functionality to query for posts from the server

--- app/src/main/java/com/assignment5/instagram/MainActivity.kt
+++ app/src/main/java/com/assignment5/instagram/MainActivity.kt
@@ -3,9 +3,13 @@ package com.assignment5.instagram
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
 
 
 /**
@@ -13,9 +17,39 @@ import android.widget.Toast
  */
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        queryForPosts()
+    }
+
+    // make a query for all posts from the server, todo: replace with something sophisticated
+    private fun queryForPosts() {
+        // specify the class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+
+        // find all post objects in the server
+        query.include(Post.KEY_USER)  // return the user object associated with each post
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(postObjects: MutableList<Post>?, e: ParseException?) {
+                if (e == null) {
+                    if (postObjects != null) {
+                        for (post in postObjects) {
+                            Log.i(TAG, "Post: ${post.getCaption()}, username: ${post.getUser()?.username}")
+                        }
+                    }
+                }
+                else {
+                    Log.e(TAG, "Error fetching posts!")
+                }
+            }
+        })
     }
 
 
@@ -24,7 +58,6 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
-
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.logout) {
