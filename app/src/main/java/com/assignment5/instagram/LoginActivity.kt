d3c2aa0b13cad25ea3cc14565f7375a041efb340
sehumah <sehumah@protonmail.com>
2022-03-23 20:33:38 -0500

    create parse object to test that the app is connecting to back4app

--- app/src/main/java/com/assignment5/instagram/LoginActivity.kt
+++ app/src/main/java/com/assignment5/instagram/LoginActivity.kt
@@ -2,14 +2,18 @@ package com.assignment5.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.parse.ParseObject
 
 
 /**
  * Contains the UI and logic to let the user login to the Parse server
  */
+
+private const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,22 @@ class LoginActivity : AppCompatActivity() {
             val etPassword = findViewById<EditText>(R.id.et_password).text.toString()
             loginUser(etUsername, etPassword)
         }
+
+        /**
+         * Testing initial connection
+         */
+        // create a ParseObject to test the connection
+        val parseObject: ParseObject = ParseObject("TestConnectionClass")
+        parseObject.put("message", "Test message from Simple-Instagram Android app. Parse is now connected!")
+        parseObject.saveInBackground {
+            if (it != null) {
+                it.localizedMessage?.let { message -> Log.e(TAG, message) }
+            } else {
+                Log.d(TAG, "Object saved!")
+            }
+        }
     }
+
 
     // make network call to log in the user if they have the right info and already have an account
     private fun loginUser(etUsername: String, etPassword: String) {
