6b17a4f7c2838a3463b9db7d30456dd2e7d98530
sehumah <sehumah@protonmail.com>
2022-03-29 14:57:27 -0500

    create adapter class for posts, extend the recyclerview's adapter and implement its members in the new post adapter class

---
+++ app/src/main/java/com/assignment5/instagram/PostAdapter.kt
@@ -0,0 +1,25 @@
+package com.assignment5.instagram
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+class PostAdapter : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+
+    inner class ViewHolder(){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        // TODO("Not yet implemented")
+    }
+}
+
